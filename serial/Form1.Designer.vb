e948c409b44ee386fc9ec5e5b241a90afb70356d
YuKiMuMa <kidosan3.14@gmail.com>
2021-07-01 13:41:02 +0900

    プロジェクト ファイルを追加します。

---
+++ serial/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.cmbPortName = New System.Windows.Forms.ComboBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM18"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(17, 16)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 29)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "接続"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(252, 299)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(132, 22)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(17, 76)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 29)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "切断"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(17, 144)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 29)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "送信"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.ImageLocation = "D:\MEGA\industrial\delta\VB\serial\serial\t.png"
+        Me.PictureBox1.Location = New System.Drawing.Point(279, 96)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(510, 445)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(329, 22)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "REST"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'cmbPortName
+        '
+        Me.cmbPortName.FormattingEnabled = True
+        Me.cmbPortName.Location = New System.Drawing.Point(160, 21)
+        Me.cmbPortName.Name = "cmbPortName"
+        Me.cmbPortName.Size = New System.Drawing.Size(121, 23)
+        Me.cmbPortName.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(926, 635)
+        Me.Controls.Add(Me.cmbPortName)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents cmbPortName As ComboBox
+End Class
---
+++ serial/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try                     '例外処理のはじまり 
+            If SerialPort1.IsOpen = True Then           'ポートはオープン済み
+                MessageBox.Show("エラー", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Exit Sub
+            End If
+            SerialPort1.PortName = cmbPortName.SelectedItem    'オープンするポート名を格納
+            SerialPort1.Open()                          'ポートオープン        
+        Catch ex As Exception           '例外処理            
+            MessageBox.Show(ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If SerialPort1.IsOpen = True Then   'ポートオープン済み
+            SerialPort1.Close()                         'ポートクローズ
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If TextBox1.Text.Length = 0 Then                '送信データが無ければエラー
+            MessageBox.Show("文字列入力エラー", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub                '処理を抜ける
+        End If
+
+        Try
+            SerialPort1.WriteLine(TextBox1.Text)       '送信バッファにデータ書き込み
+        Catch ex As Exception           '例外処理            
+            MessageBox.Show(ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim PortList As String()
+        PortList = SerialPort.GetPortNames()
+
+        cmbPortName.Items.Clear()
+
+        'シリアルポート名をコンボボックスにセットする.
+        Dim PortName As String
+        For Each PortName In PortList
+            cmbPortName.Items.Add(PortName)
+        Next PortName
+
+        If cmbPortName.Items.Count > 0 Then
+            cmbPortName.SelectedIndex = 0
+        End If
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim PortList As String()
+        PortList = SerialPort.GetPortNames()
+
+        cmbPortName.Items.Clear()
+
+        'シリアルポート名をコンボボックスにセットする.
+        Dim PortName As String
+        For Each PortName In PortList
+            cmbPortName.Items.Add(PortName)
+        Next PortName
+
+        If cmbPortName.Items.Count > 0 Then
+            cmbPortName.SelectedIndex = 0
+        End If
+    End Sub
+End Class
